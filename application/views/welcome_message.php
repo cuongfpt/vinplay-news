b3342e369fbe6c36ac14135b63609f4a0a973e04
cuongfpt <nguyencuong.fpt2013@gmail.com>
2016-09-24 16:40:13 +0700

    trang chu

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -3,86 +3,191 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+    <meta charset="utf-8">
+    <title>Welcome to CodeIgniter</title>
+    <link type="text/css" rel="stylesheet" href="public/site/css/vinplay-news.css">
 
-	<style type="text/css">
-
-	::selection { background-color: #E13300; color: white; }
-	::-moz-selection { background-color: #E13300; color: white; }
-
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
-
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-	}
-
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
-
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
-
-	#body {
-		margin: 0 15px 0 15px;
-	}
-
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-
-	#container {
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
 </head>
 <body>
 
 <div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+    <div class="header">
+        <div class="header_content">
+            <div class="header_left">
+                <div class="div_game">
+                    <ul>
+                        <li><a href="#" class="game_dangian"></a></li>
+                        <li><a href="#" class="game_slot"></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="header_right">
+                <ul>
+                    <li><a href="#" class="btn_login"></a></li>
+                    <li><a href="#" class="btn_reg"></a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="nav">
+            <div class="nav_content">
+                <ul>
+                    <li class="icon_home"><a href="/"></a></li>
+                    <li><a href="#">tin tức</a></li>
+                    <li><a href="#">chính sách</a></li>
+                    <li><a href="#">thưởng vip</a></li>
+                    <li><a href="#">vinh danh</a></li>
+                    <li><a href="#">vòng quay may mắn</a></li>
+                    <li><a href="#">hỗ trợ</a></li>
+                    <li><a href="#">hướng dẫn</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="content">
+        <div class="content_page">
+            <div class="content_left">
+                <ul>
+                    <li>
+                        <div class="item_news">
+                            <div class="div_images">
+                                <img src="public/site/images/news.png">
+                            </div>
+                            <div class="div_content">
+                                <div class="title_new">Samsung Việt Nam châm chọc iPhone 7 gây tranh cãi</div>
+                                <div class="date">24/09/2016</div>
+                                <div class="description">“Đen bóng ư” Có từ lâu rồi”, fanpage chính thức của Samsung
+                                    Việt
+                                    Nam vừa đăng tải kèm hình chiếc Galaxy S7 edge màu black sapphire.
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="item_news">
+                            <div class="div_images">
+                                <img src="public/site/images/news.png">
+                            </div>
+                            <div class="div_content">
+                                <div class="title_new">Samsung Việt Nam châm chọc iPhone 7 gây tranh cãi</div>
+                                <div class="date">24/09/2016</div>
+                                <div class="description">“Đen bóng ư” Có từ lâu rồi”, fanpage chính thức của Samsung
+                                    Việt
+                                    Nam vừa đăng tải kèm hình chiếc Galaxy S7 edge màu black sapphire.
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="item_news">
+                            <div class="div_images">
+                                <img src="public/site/images/news.png">
+                            </div>
+                            <div class="div_content">
+                                <div class="title_new">Samsung Việt Nam châm chọc iPhone 7 gây tranh cãi</div>
+                                <div class="date">24/09/2016</div>
+                                <div class="description">“Đen bóng ư” Có từ lâu rồi”, fanpage chính thức của Samsung
+                                    Việt
+                                    Nam vừa đăng tải kèm hình chiếc Galaxy S7 edge màu black sapphire.
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+                <div class="pagination">
+                    <ul>
+                        <li><a href="#">Trang trước</a></li>
+                        <li><a class="current" href="#">1</a></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                        <li><a href="#">Trang sau</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="content_right">
+                <div class="top10">
+                    <div class="div_victory">
+                        <div class="title_top10">
+                            <table>
+                                <tr>
+                                    <td class="w47">STT</td>
+                                    <td class="w130">Tên nhân vật</td>
+                                    <td class="w110">Điểm thắng</td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div class="content_top10">
+                            <table>
+                                <tr>
+                                    <td class="w47">1</td>
+                                    <td class="w130">hiepnm4181</td>
+                                    <td class="w110">34,917,520</td>
+                                </tr>
+                                <tr>
+                                    <td class="w47">2</td>
+                                    <td class="w130">hiepnm4181</td>
+                                    <td class="w110">34,917,520</td>
+                                </tr>
+                                <tr>
+                                    <td class="w47">3</td>
+                                    <td class="w130">hiepnm4181</td>
+                                    <td class="w110">34,917,520</td>
+                                </tr>
+                                <tr>
+                                    <td class="w47">4</td>
+                                    <td class="w130">hiepnm4181</td>
+                                    <td class="w110">34,917,520</td>
+                                </tr>
+                                <tr>
+                                    <td class="w47">5</td>
+                                    <td class="w130">hiepnm4181</td>
+                                    <td class="w110">34,917,520</td>
+                                </tr>
+                                <tr>
+                                    <td class="w47">6</td>
+                                    <td class="w130">hiepnm4181</td>
+                                    <td class="w110">34,917,520</td>
+                                </tr>
+                                <tr>
+                                    <td class="w47">7</td>
+                                    <td class="w130">hiepnm4181</td>
+                                    <td class="w110">34,917,520</td>
+                                </tr>
+                                <tr>
+                                    <td class="w47">8</td>
+                                    <td class="w130">hiepnm4181</td>
+                                    <td class="w110">34,917,520</td>
+                                </tr>
+                                <tr>
+                                    <td class="w47">9</td>
+                                    <td class="w130">hiepnm4181</td>
+                                    <td class="w110">34,917,520</td>
+                                </tr>
+                                <tr>
+                                    <td class="w47">10</td>
+                                    <td class="w130">hiepnm4181</td>
+                                    <td class="w110">34,917,520</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-	</div>
-
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+    </div>
+    <div class="footer">
+        <div class="footer_content">
+            <div class="footer_top">
+                <ul>
+                    <li><a href="#">Tin tức</a></li>
+                    <li><a href="#">chính sách</a></li>
+                    <li><a href="#">thưởng vip</a></li>
+                    <li><a href="#">vinh danh</a></li>
+                    <li><a href="#">hố trợ</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </div>
 
 </body>
