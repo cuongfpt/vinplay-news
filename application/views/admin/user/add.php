c887bd195bdc149d6f34fcb358a677bc68614385
vinhct <vinhfalcao@gmail.com>
2018-06-01 14:44:56 +0700

    add

--- application/views/admin/user/add.php
+++ application/views/admin/user/add.php
@@ -12,87 +12,102 @@
                 </div>
                 <!-- /.box-header -->
                 <form id="form" class="form" enctype="multipart/form-data" method="post" action="">
-                    <fieldset
-                <div class="box-body">
-                    <div class="form-group ">
-                        <div class="row">
-                            <div class="col-sm-3"></div>
-                            <label for="inputEmail3" class="col-sm-2 control-label">Tên đăng nhập:</label>
+                    <fieldset>
+                        <div class="box-body">
+                            <div class="form-group ">
+                                <div class="row">
+                                    <div class="col-sm-3"></div>
+                                    <label for="inputEmail3" class="col-sm-2 control-label">Tên đăng nhập:</label>
 
-                            <div class="col-sm-3">
-                                <input type="text" class="form-control" name="username">
+                                    <div class="col-sm-3">
+                                        <input type="text" class="form-control" name="username">
+                                    </div>
+                                    <div class="col-sm-4"><label class="control-label" for="inputError"
+                                                                 style="color: #ff0000"><?php echo form_error('username') ?></label>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="col-sm-4"><label class="control-label" for="inputError" style="color: #ff0000"><?php echo form_error('username')?></label></div>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="row">
-                            <div class="col-sm-3"></div>
-                            <label class="col-sm-2 control-label">Mật khẩu:</label>
+                            <div class="form-group">
+                                <div class="row">
+                                    <div class="col-sm-3"></div>
+                                    <label class="col-sm-2 control-label">Mật khẩu:</label>
 
-                            <div class="col-sm-3">
-                                <input type="password" class="form-control" name="password">
+                                    <div class="col-sm-3">
+                                        <input type="password" class="form-control" name="password">
+                                    </div>
+                                    <div class="col-sm-4"><label class="control-label" for="inputError"
+                                                                 style="color: #ff0000"><?php echo form_error('password') ?></label>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="col-sm-4"><label class="control-label" for="inputError" style="color: #ff0000"><?php echo form_error('password')?></label></div>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="row">
-                            <div class="col-sm-3"></div>
-                            <label class="col-sm-2 control-label">Nhập lại mật khẩu:</label>
+                            <div class="form-group">
+                                <div class="row">
+                                    <div class="col-sm-3"></div>
+                                    <label class="col-sm-2 control-label">Nhập lại mật khẩu:</label>
 
-                            <div class="col-sm-3">
-                                <input type="password" class="form-control" name="repassword">
+                                    <div class="col-sm-3">
+                                        <input type="password" class="form-control" name="repassword">
+                                    </div>
+                                    <div class="col-sm-4"><label class="control-label" for="inputError"
+                                                                 style="color: #ff0000"><?php echo form_error('repassword') ?></label>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="col-sm-4"><label class="control-label" for="inputError" style="color: #ff0000"><?php echo form_error('repassword')?></label></div>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="row">
-                            <div class="col-sm-3"></div>
-                            <label class="col-sm-2 control-label">Email:</label>
+                            <div class="form-group">
+                                <div class="row">
+                                    <div class="col-sm-3"></div>
+                                    <label class="col-sm-2 control-label">Email:</label>
 
-                            <div class="col-sm-3">
-                                <input type="text" class="form-control" name="email">
+                                    <div class="col-sm-3">
+                                        <input type="text" class="form-control" name="email">
+                                    </div>
+                                    <div class="col-sm-4"><label class="control-label" for="inputError"
+                                                                 style="color: #ff0000"><?php echo form_error('email') ?></label>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="col-sm-4"><label class="control-label" for="inputError" style="color: #ff0000"><?php echo form_error('email')?></label></div>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="row">
-                            <div class="col-sm-3"></div>
-                            <label class="col-sm-2 control-label">Điện thoại:</label>
+                            <div class="form-group">
+                                <div class="row">
+                                    <div class="col-sm-3"></div>
+                                    <label class="col-sm-2 control-label">Điện thoại:</label>
 
-                            <div class="col-sm-3">
-                                <input type="text" class="form-control" name="phone">
+                                    <div class="col-sm-3">
+                                        <input type="text" class="form-control" name="phone">
+                                    </div>
+                                    <div class="col-sm-4"><label class="control-label" for="inputError"
+                                                                 style="color: #ff0000"><?php echo form_error('phone') ?></label>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="col-sm-4"><label class="control-label" for="inputError" style="color: #ff0000"><?php echo form_error('phone')?></label></div>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="row">
-                            <div class="col-sm-3"></div>
-                            <label class="col-sm-2 control-label">Địa chỉ:</label>
+                            <div class="form-group">
+                                <div class="row">
+                                    <div class="col-sm-3"></div>
+                                    <label class="col-sm-2 control-label">Địa chỉ:</label>
 
-                            <div class="col-sm-3">
-                                <input type="text" class="form-control" name="address">
+                                    <div class="col-sm-3">
+                                        <input type="text" class="form-control" name="address">
+                                    </div>
+                                    <div class="col-sm-4"><label class="control-label" for="inputError"
+                                                                 style="color: #ff0000"><?php echo form_error('address') ?></label>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="col-sm-4"><label class="control-label" for="inputError" style="color: #ff0000"><?php echo form_error('address')?></label></div>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="row">
-                            <div class="col-sm-3"></div>
+                            <div class="form-group">
+                                <div class="row">
+                                    <div class="col-sm-3"></div>
 
-                            <div class="col-sm-2"></div>
-                            <div class="col-sm-3">
-                                <input type="submit" value="Thêm mới" name="submit" class="btn btn-primary pull-right">
+                                    <div class="col-sm-2"></div>
+                                    <div class="col-sm-3">
+                                        <input type="submit" value="Thêm mới" name="submit"
+                                               class="btn btn-primary pull-right">
+                                    </div>
+                                    <div class="col-sm-4"></div>
+                                </div>
                             </div>
-                            <div class="col-sm-4"></div>
-                        </div>
-                    </div>
 
-                </div>
+                        </div>
+                    </fieldset>
+                </form>
             </div>
         </div>
     </div>
